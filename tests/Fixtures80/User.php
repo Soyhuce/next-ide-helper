3f3976b30863fe78bdc96cfc291d9d11f0328e5b
bastien-phi <bastien-phi@users.noreply.github.com>
2021-09-03 06:29:09 +0000

    Fix styling

--- tests/Fixtures80/User.php
+++ tests/Fixtures80/User.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    public function intOrString(): int | string
+    public function intOrString(): int|string
     {
         return 'foo';
     }
