a9ea753fb158f68523c2c46e70bbc72d3377703d
Bastien Philippe <bastien.philippe@soyhuce.fr>
2021-09-10 16:19:56 +0200

    Prefix IdeHelper classes with __ to make them less searchable

--- src/Support/Output/IdeHelperClass.php
+++ src/Support/Output/IdeHelperClass.php
@@ -8,14 +8,18 @@ class IdeHelperClass
 {
     public static function eloquentBuilder(string $modelFqcn): string
     {
+        $classBasename = class_basename($modelFqcn);
+
         return (string) Str::of($modelFqcn)->trim('\\')
-            ->prepend('\\IdeHelper\\')->append('Query');
+            ->beforeLast($classBasename)
+            ->prepend('\\IdeHelper\\')
+            ->append("__${classBasename}Query");
     }
 
     public static function relation(string $modelFqcn, string $relationName): string
     {
         return (string) Str::of($modelFqcn)->trim('\\')
             ->prepend('\\IdeHelper\\')
-            ->append(Str::of($relationName)->studly()->prepend('\\'));
+            ->append(Str::of($relationName)->studly()->prepend('\\__'));
     }
 }
