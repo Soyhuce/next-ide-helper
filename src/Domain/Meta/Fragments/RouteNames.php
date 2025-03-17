40682eecc746878ddcc421e1843948d0ffeb454a
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-03-17 15:19:03 +0100

    Route name method completion

---
+++ src/Domain/Meta/Fragments/RouteNames.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace Soyhuce\NextIdeHelper\Domain\Meta\Fragments;
+
+use Illuminate\Routing\Redirector;
+use Illuminate\Routing\RouteCollection;
+use Illuminate\Routing\RouteCollectionInterface;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
+use Soyhuce\NextIdeHelper\Contracts\MetaFragment;
+use Soyhuce\NextIdeHelper\Domain\Meta\MetaCallable;
+use Soyhuce\NextIdeHelper\Support\Output\PhpstormMetaFile;
+
+class RouteNames implements MetaFragment
+{
+    public function add(PhpstormMetaFile $metaFile): void
+    {
+        $routeNames = $this->resolveRouteNames();
+
+        $metaFile->registerArgumentSet('routes', $routeNames);
+
+        foreach ($this->methods() as $method) {
+            $metaFile->expectedArgumentsFromSet($method, 'routes');
+        }
+    }
+
+    /**
+     * @return array<int, MetaCallable>
+     */
+    private function methods(): array
+    {
+        return [
+            new MetaCallable([Redirector::class, 'route'], 0),
+            new MetaCallable([URL::class, 'signedRoute'], 0),
+            new MetaCallable([URL::class, 'temporarySignedRoute'], 0),
+            new MetaCallable([RouteCollection::class, 'getByName'], 0),
+            new MetaCallable([RouteCollectionInterface::class, 'getByName'], 0),
+            new MetaCallable([Router::class, 'respondWithRoute'], 0),
+            new MetaCallable([Route::class, 'respondWithRoute'], 0),
+            new MetaCallable('route', 0),
+            new MetaCallable('to_route', 0),
+        ];
+    }
+
+    /**
+     * @return Collection<int, string>
+     */
+    private function resolveRouteNames(): Collection
+    {
+        return Collection::make(Route::getRoutes()->getRoutesByName())
+            ->keys()
+            ->sort()
+            ->values();
+    }
+}
